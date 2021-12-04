d54873ac34ea1971e935a070372df9ac015b132d
Danny Putnam <dannykputnam@gmail.com>
2021-12-04 12:29:41 -0700

    user add, and index views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :password, :first_name, :last_name)
   end
 end
--- app/javascript/components/UserNew.js
+++ app/javascript/components/UserNew.js
@@ -3,23 +3,59 @@ import React from 'react';
 const UserNew = ({ user }) => {
 
   // attr that your obj 
-  const { name } = user 
+  const { first_name, last_name, email, password } = user 
   // in case there are error it saves the user input
   // one per attr
-  const defaultName = name ? name : ""
+  const defaultFirstName = first_name ? first_name : ""
+  const defaultLastName = last_name ? last_name : ""
+  const defaultEmail = email ? email : ""
+  const defaultPassword = password ? password : ""
   return (
     <>
       <h1>Create new User</h1>
       <form action="/users" method="post">
         <div className="form-group">
-          <label>Name</label>
+          <label>First Name:  </label>
           <input
             className="form-control col-md-6"
             type="text"
             placeholder="User name"
             required
-            defaultValue={defaultName}
-            name="user[name]"
+            defaultValue={defaultFirstName}
+            name="user[first_name]"
+          />
+        </div>
+        <div className="form-group">
+          <label>Last Name:  </label>
+          <input
+            className="form-control col-md-6"
+            type="text"
+            placeholder="User name"
+            required
+            defaultValue={defaultLastName}
+            name="user[last_name]"
+          />
+        </div>
+        <div className="form-group">
+          <label>Email:  </label>
+          <input
+            className="form-control col-md-6"
+            type="text"
+            placeholder="User Email"
+            required
+            defaultValue={defaultEmail}
+            name="user[email]"
+          />
+        </div>
+        <div className="form-group">
+          <label>Password:  </label>
+          <input
+            className="form-control col-md-6"
+            type="text"
+            placeholder="User password"
+            required
+            defaultValue={defaultPassword}
+            name="user[password]"
           />
         </div>
         <button type="submit" className="btn btn-primary">Add User</button>
